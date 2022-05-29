a94751855639d9dad3db5bf2b363e7dab8a9a7f1
李金轩 <43086817+Li-Jinxuan@users.noreply.github.com>
2022-05-29 20:05:34 +0800

    2022年5月29日20:05:28

--- 门票/打砖块关卡编辑器/scene/title/scene_title.js
+++ 门票/打砖块关卡编辑器/scene/title/scene_title.js
@@ -11,5 +11,6 @@ class SceneTitle extends GuaScene {
         // draw labels
         this.game.context.fillText('按 k 开始游戏', 100, 190)
         this.game.context.fillText('按 f 发射', 100, 210)
+        this.game.context.fillText('按 123 切换关卡', 100, 230)
     }
 }
